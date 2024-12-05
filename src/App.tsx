833a2ad54f43c61afbcaa1db5c9d1d1fd38044ec
igor vicente <igor_machado05@hotmail.com>
2024-12-05 11:14:37 -0300

    teste

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export const App = () => {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+        <iframe
+          loading="lazy"
+          src="https://iframe.mediadelivery.net/embed/348589/c28c6495-0cf7-4ece-bf40-d129d5b96a13?autoplay=false&loop=false&muted=false&preload=false&responsive=true"
+          allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;"
+          allowFullScreen
+          className="w-[50%] h-[50%] bg-black"
+        ></iframe>
+      </div>
+    </>
+  );
+};
