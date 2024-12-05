b45bf660b243baf93a2d06ef238c2b5622c21ee9
igor vicente <igor_machado05@hotmail.com>
2024-12-05 15:19:42 -0300

    config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ export const App = () => {
     <>
       <div className="h-screen flex items-center justify-center">
         <iframe
-          loading="lazy"
+          // loading="lazy"
           src="https://iframe.mediadelivery.net/embed/348589/c28c6495-0cf7-4ece-bf40-d129d5b96a13?autoplay=false&loop=false&muted=false&preload=false&responsive=true"
           allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;"
           allowFullScreen
